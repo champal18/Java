94a7d2f3300e6a4325a509890047e263a68fe803
champal18 <juampacabezon@hotmail.com>
2016-06-10 16:44:56 -0300

    Update Todo! :)

---
+++ WorkSpace/Proyecto_ORM/src/modelo/Admin.java
@@ -0,0 +1,31 @@
+package modelo;
+
+import java.time.LocalDate;
+
+public class Admin extends Persona {
+
+  private String nomAdmin;
+
+  public Admin(String nombre, String apellido, String nomAdmin, String pass, String domicilio, Integer dni, LocalDate fecha, Sexo sexo, String mail) 
+  {
+	  
+  	this.setNombre(nombre);
+  	this.setApellido(apellido);
+  	this.setNomAdmin(nomAdmin);
+  	this.setPass(pass);
+  	this.setDomicilio(domicilio);
+  	this.setDni(dni);
+  	this.setFechaNac(fecha);
+  	this.setSexo(sexo);
+  	this.setMail(mail);
+  }
+
+	public String getNomAdmin() {
+		return nomAdmin;
+	}
+	
+	public void setNomAdmin(String nomAdmin) {
+		this.nomAdmin = nomAdmin;
+	}
+
+}
